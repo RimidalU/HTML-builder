900a9baedd137a7bd4e38c599b407b7482ccfa6f
RimidalU <stankevichuuladimiru@gmail.com>
2021-11-09 01:01:57 +0300

    feat: add 5 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,15 +1,25 @@
 const fs = require('fs');
+const fsProm = require('fs/promises');
 const path = require('path');
+const bundel = [];
 
+const inputPath = path.resolve(__dirname, "styles");
+const outputPath = path.resolve(__dirname, "project-dist");
 
-
-
-
-
-
-
-
-
+  fs.readdir(inputPath, {withFileTypes: true}, (error, files) => {
+    if(error) {
+        console.log('Error:', error);}
+    else {
+      files.forEach(file => {
+        if (file.isFile() && file.name.split('.')[1] === 'css') {
+            bundel.push(fsProm.readFile(path.resolve(inputPath, file.name)));
+            Promise.all(bundel).then(value => {
+            fsProm.writeFile(path.resolve(outputPath, 'bundle.css'), value.join('\n'));
+          });
+        }
+      });
+    }
+  });
 
 
 
